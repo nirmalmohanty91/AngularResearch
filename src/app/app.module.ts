728b08c4b41652e88a890e7406ddde26d545ce6d
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2019-02-18 01:36:25 +0530

    working with array of objects in a form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     AppComponent,
     UserComponent,
     ContactFormComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
