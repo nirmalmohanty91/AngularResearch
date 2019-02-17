26ec741c28bcabb75353e8fd3d23b78a6c5ee7c8
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2019-02-18 01:06:56 +0530

    Asynch loader

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -7,4 +7,15 @@ export class UserNameValidators {
         }
         return null;
     }
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors|null>{
+            return new Promise((resolve, reject)=>{
+                    setTimeout(()=>{
+                        if(control.value === 'nirmal'){
+                            resolve( {shouldBeUnique: true})
+                        }else{
+                            resolve( null);
+                        }
+                    }, 2000);
+                });
+    }
 }
